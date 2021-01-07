7b29e53c86ebd96ffa43376a0f36ed570da454a2
Eliezer Valenzuela <vzla0094@gmail.com>
2021-01-06 19:22:35 -0700

    added voting functionality

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,62 @@
 import React from "react";
+import { AUTH_TOKEN, LINKS_PER_PAGE } from "../constants";
+import { timeDifferenceForDate } from "../utils";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
-const Link = ({ link }) => {
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+const Link = ({ link, index }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  const [vote] = useMutation(VOTE_MUTATION, { variables: { linkId: link.id } });
+
+  const take = LINKS_PER_PAGE;
+  const skip = 0;
+  const orderBy = { createdAt: "desc" };
+
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <div
+            className="ml1 gray f11"
+            style={{ cursor: "pointer" }}
+            onClick={vote}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        {authToken && (
+          <div className="f6 lh-copy gray">
+            {link.votes.length} votes | by{" "}
+            {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+            {timeDifferenceForDate(link.createdAt)}
+          </div>
+        )}
       </div>
     </div>
   );
